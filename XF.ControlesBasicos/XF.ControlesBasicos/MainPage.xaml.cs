de487649d626e5716ec8526d05c0c895d059c63a
Satoru <satoru_kishi@hotmail.com>
2018-06-17 22:32:59 -0300

    Add project files.

---
+++ XF.ControlesBasicos/XF.ControlesBasicos/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using XF.ControlesBasicos.Model;
+
+namespace XF.ControlesBasicos
+{
+	public partial class MainPage : ContentPage
+	{
+        private Email email;
+
+		public MainPage()
+		{
+			InitializeComponent();
+
+            if (email == null)
+            {
+                email = new Email();
+            }
+		}
+
+        private void btnEnviar_Clicked(object sender, EventArgs e)
+        {
+            if (email.AceitaReceber && !String.IsNullOrEmpty(email.Conta))
+            {
+                DisplayAlert("Sucesso", $"E-mail enviado para {email.Conta}", "OK");
+            }
+            else
+            {
+                DisplayAlert("Ops", "O proprietário desta conta de e-mail não deseja receber informativos.", "OK");
+            }
+        }
+
+        private async void btnConfiguracao_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ConfigPage() { BindingContext = email });
+        }
+    }
+}
---
+++ XF.ControlesBasicos/XF.ControlesBasicos/Model/Email.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace XF.ControlesBasicos.Model
+{
+    public class Email : INotifyPropertyChanged
+    {
+        private bool _aceitaReceber;
+        public bool AceitaReceber
+        {
+            get { return _aceitaReceber; }
+            set {
+                _aceitaReceber = value;
+                if (!_aceitaReceber)
+                {
+                    Conta = string.Empty;
+                }
+            }
+        }
+
+        private string _conta;
+        public string Conta {
+            get { return _conta; }
+            set
+            {
+                _conta = value;
+                EventPropertyChanged();
+            }
+        }
+
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void EventPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion INotifyPropertyChanged
+
+    }
+}
